6668709e35fbc48f216cc948ee30c80da9207781
alex <alexmlg78@gmail.com>
2019-04-02 21:14:58 -0600

    datos de firebase

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,14 @@
+
+
+
+export interface InfoPagina{
+  titulo?: String;
+  Email?:  String;
+  Nombre_corto?: String;
+  Pagina_autor?: String;
+  Facebook?: String;
+  Twitter?: String;
+  Instagram?: String;
+  equipo_trabajo?:any [];
+
+}
--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -1,21 +1,65 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+
+import {InfoPagina} from "../interfaces/info-pagina.interface";
 @Injectable({
   providedIn: 'root'
 })
 
 export class InformacionService {
-info:any={};
-cargada:boolean= false;
+  info: InfoPagina = {};
+  cargada = false;
 
-  constructor(public http: HttpClient) {
+  Equipo: any[] = [];
+/*  Equipo: InfoEquipo =[];
+info: InfoPagina = {};
+cargada:boolean= false;
+cargada_sobre_nosotros:boolean= false;
+//Equipo: InfoEquipo = {};
+*/
+
+  constructor(private http: HttpClient) {
+
+/*
+    this.cargainfo();
+    this.carga_sobre_nosotros();
+
+  }*/
+
+
+  this.cargarInfo();
+  this.cargarEquipo();
+
+  }
+
+
+
+  private cargarInfo (){
     this.http.get("assets/data/info.pagina.json")
-             .subscribe( data =>{
+             /* .subscribe( data =>{
              console.log(data );
              this.cargada=true;
-             this.info=data; 
+             this.info=data;
+             })*/
+             //leer archivo json
+             .subscribe((resp: InfoPagina) =>{
+                this.cargada=true;
+                this.info=resp;
+
              })
   }
+
+public cargarEquipo(){
+  this.http.get("https://angular-d6e6b.firebaseio.com/Equipo.json")
+  .subscribe( (resp: any[]) => {
+
+    this.Equipo = resp;
+    //console.log(resp);
+  });
+
+
+}
+
 
 
 
